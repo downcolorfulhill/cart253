ddbd9113e4575e3c29fca9e18fe6b4c81f7a91ba
downcolorfulhill <bmcintyre10@gmail.com>
2026-09-25 15:26:14 -0400

    changed all circles to rectangles of varying shapes

--- topics/prototypes/instructions/instructions 1/js/script.js
+++ topics/prototypes/instructions/instructions 1/js/script.js
@@ -9,14 +9,14 @@
  * Creates the canvas
  */
 function setup() {
-  createCanvas(400, 800);
+  createCanvas(800, 800);
 }
 
 /**
  * Draws the concentric circles
  */
 function draw() {
-  background("#87ceeb");
+  background("#257524");
   
   // We want this to apply to *all* circles, so it can go out here
   noStroke();
@@ -24,32 +24,32 @@ function draw() {
   // Begin circles diminishing in size and darkness
   push();
   fill(0);
-  ellipse(200, 200, 300);
+  rect(200, 200, 300, 5);
   pop();
   
   push();
   fill(50);
-  ellipse(200, 200, 250);
+  rect(200, 200, 250, 66);
   pop();
   
   push();
   fill(100);
-  ellipse(200, 200, 200);
+  rect(200, 200, 200, 400);
   pop();
   
   push();
   fill(150);
-  ellipse(200, 200, 150);
+  rect(200, 200, 150, 68);
   pop();
   
   push();
   fill(200);
-  ellipse(200, 200, 100);
+  rect(200, 200, 100, 500);
   pop();
   
   push();
   fill(250);
-  ellipse(200, 200, 50);
+  rect(200, 200, 50, 300);
   pop();
   
   // Or you could do it like this (we will learn this later)
